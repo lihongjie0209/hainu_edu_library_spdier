db2fa43377aa0ae92f910ae415e8db07c260f54a
lihongjie0209 <982264618@qq.com>
2017-02-04 23:31:19 +0800

    change reqeust url

--- hainu_library/spiders/library_spider.py
+++ hainu_library/spiders/library_spider.py
@@ -9,7 +9,7 @@ class LibrarySpider(scrapy.Spider):
     name = 'lib_spider'
 
     def start_requests(self):
-        urls =('http://210.37.32.7/opac/search?q=*%3A*&searchType=standard&isFacet=true&view=standard&rows=100&hasholding=1&searchWay0=marc&q0=&logical0=AND&page={}'.format(page) for page in range(1,2))
+        urls =('http://210.37.32.7/opac/search?q=*%3A*&rows=100&&page={}'.format(page) for page in range(1,3))
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse) 
@@ -19,13 +19,13 @@ class LibrarySpider(scrapy.Spider):
         bookmetas = response.xpath('//div[@class="bookmeta"]')
         for meta in bookmetas:
             l = BookItemLoader(item=BookItem(), selector=meta, response=response)
-            l.add_xpath('bookrecno', '@bookrecno')
+            l.add_xpath('_id', '@bookrecno')
             l.add_xpath('title', './/span[@class="bookmetaTitle"]/a/text()')
             l.add_xpath('author', './/a[contains(@href, "author")]/text()')
             l.add_xpath('publisher', './/a[contains(@href, "publisher")]/text()')
             l.add_xpath('pub_date', './/a[contains(@href, "publisher")]/following-sibling::text()', re=r'出版日期: (.+)')
             # item = l.load_item()
-            yield scrapy.Request(url='http://210.37.32.7/opac/api/holding/'+l.get_output_value('bookrecno'), callback=self.parse_json, meta={'loader':l})
+            yield scrapy.Request(url='http://210.37.32.7/opac/api/holding/'+l.get_output_value('_id'), callback=self.parse_json, meta={'loader':l})
 
     def parse_json(self, response):
         l = response.meta['loader']
