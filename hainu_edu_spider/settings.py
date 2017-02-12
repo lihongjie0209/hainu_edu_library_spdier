a1bddd66a2a0798794235063b4334ad7d0a8e2e3
lihongjie0209 <lihongjie0209@gmail.com>
2017-02-13 03:42:58 +0800

    clean settings

--- hainu_edu_spider/settings.py
+++ hainu_edu_spider/settings.py
@@ -16,64 +16,18 @@ BOT_NAME = 'hainu_edu_spider'
 SPIDER_MODULES = ['hainu_edu_spider.spiders']
 NEWSPIDER_MODULE = 'hainu_edu_spider.spiders'
 
+# 测试时使用缓存以及高并发访问
+# HTTPCACHE_ENABLED = True
+# CONCURRENT_REQUESTS = 100
+# CONCURRENT_REQUESTS_PER_DOMAIN = 100
 
-HTTPCACHE_ENABLED = True
-CONCURRENT_REQUESTS = 100
-CONCURRENT_REQUESTS_PER_DOMAIN = 100
 # Mongodb Setting
 Mongo_URI = 'mongodb://127.0.0.1:27017'
 
 
-# logging setting
-
-# LOG_FILE = 
-# LOG_LEVEL = 
-
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'hainu_library (+http://www.yourdomain.com)'
-
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'hainu_library.middlewares.HainuLibrarySpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'hainu_library.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
-
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
@@ -82,18 +36,6 @@ ITEM_PIPELINES = {
    'hainu_edu_spider.pipelines.MongoPipeline' : 400
 }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
 
 
 
